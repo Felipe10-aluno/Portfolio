5aaa46258d059276ab8f8b2b0c254bbb9c3883c6
Felipe10-aluno <felipe_lima-pereira@estudante.sesisenai.org.br>
2025-03-11 16:01:26 -0300

    Delete assets directory

--- assets/vendor/php-email-form/validate.js
+++
@@ -1,8 +0,0 @@
-function handleSubmit(event) {
-  event.preventDefault(); 
-
-  setTimeout(function() {
-    document.querySelector('.sent-message').style.display = 'block';
-    document.querySelector('.sent-message').classList.add('fade-in');
-  }, 500);
-}
